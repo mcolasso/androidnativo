c16a71a91d055fa27fef1801a3d991caf51aa505
colassomatheus <matheuscolasso2006@gmail.com>
2026-05-28 19:49:39 -0300

    Refactor MainActivity to use Column layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,58 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android!!!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            // A Column já garante o alinhamento à direita (End) e no meio (Center)
+            Column(
+                modifier = Modifier
+                    .background(color = Color.Blue)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.End
+            ) {
+                // Estado compartilhado (iniciando vazio para ficar melhor na tela)
+                var vlrDoTextField by remember { mutableStateOf("") }
+
+                // Nomes importantes na história do computador (incluindo mulheres)
+                Text("Radia Perlman")
+                Text("Vint Cerf")
+                Text("Hedy Lamarr")
+                Text("Tim Berners-Lee")
+                Text("Evelyn Boyd Granville")
+
+                // Campos de texto replicados
+                TextField(vlrDoTextField, {
+                    vlrDoTextField = it
+                })
+
+                OutlinedTextField(vlrDoTextField, {
+                    Log.i("###", it) // Mantive o seu Log!
+                    vlrDoTextField = it
+                })
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
